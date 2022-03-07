979f88f2f6cecbaedb635e08166d0b8b68731deb
paridhipathak <66423076+paridhipathak@users.noreply.github.com>
2022-03-07 19:12:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      name: "Paridhi",
+      age: 30,
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgpALMedi9-917o1f68SR7WMpBm3pPP8LQxA&usqp=CAU",
+    };
+  },
+});
+
+app.mount("#assignment");
